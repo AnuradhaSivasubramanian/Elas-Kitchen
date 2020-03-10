85e39d001ced7c47942da44fcba3ed32d35df69f
Lígia Souza <ligia.o.souza@gmail.com>
2020-03-10 10:30:18 +0100

    handling recipe.js

--- src/component/Recipe.js
+++ src/component/Recipe.js
@@ -1,36 +1,39 @@
 import React from "react";
+import "./css/recipe.css"
 class Recipe extends React.Component {
   state = {
-    recipeData: {},
-    isloaded: false
+    recipeData: {    },
+    isLoaded:false
   };
-  componentDidUpdate(prevProps) {
-    if (this.props.recipes !== prevProps.recipes) {
-      this.setState({
-        recipeData: this.props.recipes,
-        isloaded: true
-      });
-    }
-  }
+  componentDidMount() {
+    
+      this.setState({recipeData: this.props.recipes})
+      this.setState({isLoaded:true})
+    }  
+
+
   render() {
     return (
       <div>
-        
-        <figure>
+        {this.state.isLoaded?
+        <div>
+           <figure>
           <img src={this.state.recipeData.image} alt="testimage"></img>
           <figcaption>{this.state.recipeData.title}</figcaption>
         </figure>
          <h3> Instructions</h3>
         <p>
-          {this.state.recipeData.analyzedInstructions[0].steps.map(
+        {/*   {this.state.recipeData.analyzedInstructions[0].steps.map(
             (item, index) => (
               <p key={index}>{item.step}</p>
             )
-          )}
+          )} */}
         </p>
-        <p>This recipe is from - {this.state.recipeData.creditsText}</p> 
+        <p>This recipe is from - {this.state.recipeData.creditsText}</p>  
+        </div>
+     : null}
       </div>
-    )} }
+    )}} 
   
 
 export default Recipe;
