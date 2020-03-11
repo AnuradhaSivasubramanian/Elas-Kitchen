8d8865ab552c147db8e52e1db37696b2fd8d8bc6
anu radha <anu.sivasubramanian@gmail.com>
2020-03-11 17:17:27 +0100

    corrected few css

--- src/component/buzzQuestions.js
+++ src/component/buzzQuestions.js
@@ -1,101 +1,118 @@
-import potato from "./images/potato.jpg"
-import pasta from "./images/pasta.jpeg"
-import rice from "./images/rice.jpeg"
-import flour from "./images/flour.jpg"
-import placeholder from "./images/placeholder.jpg"
-import paprika from "./images/paprika.jpg"
-import peas from "./images/peas.jpg"
-import abobora from "./images/abobora.jpg"
-import cheese from "./images/cheese.jpg"
-import milk from "./images/milk.jpeg"
-import eggs from "./images/eggs.jpeg"
-import butter from "./images/butter.jpeg"
-import chili from "./images/chili.jpeg"
-import garlic from "./images/garlic.jpeg"
-import onions from "./images/onions.jpeg"
-import ginger from "./images/ginger.jpeg"
-import pork from "./images/pork.jpeg"
-import beef from "./images/beef.jpeg"
-import chicken from "./images/chicken.jpeg"
-import salmon from "./images/fish.jpeg"
+import potato from "./images/potato.jpg";
+import pasta from "./images/pasta.jpeg";
+import rice from "./images/rice.jpeg";
+import flour from "./images/flour.jpg";
+import placeholder from "./images/placeholder.jpg";
+import paprika from "./images/paprika.jpg";
+import peas from "./images/peas.jpg";
+import abobora from "./images/abobora.jpg";
+import cheese from "./images/cheese.jpg";
+import milk from "./images/milk.jpeg";
+import eggs from "./images/eggs.jpeg";
+import butter from "./images/butter.jpeg";
+import chili from "./images/chili.jpeg";
+import garlic from "./images/garlic.jpeg";
+import onions from "./images/onions.jpeg";
+import ginger from "./images/ginger.jpeg";
+import pork from "./images/pork.jpeg";
+import beef from "./images/beef.jpeg";
+import chicken from "./images/chicken.jpeg";
+import salmon from "./images/fish.jpeg";
 const buzzQuestions = [
-    {
-      question: "Which carbs do you have?",
-      item1:{
-        value: "potatoes",
-        image:potato},
-      item2: {
-        value:"spaghetti",
-        image:pasta},
-        item3: {
-          value:"jasmine rice",
-          image:rice},
-          item4: {
-            value:"flour",
-            image:flour}
-      
+  {
+    question: "Which of these grains/carbs do you have?",
+    item1: {
+      value: "potatoes",
+      image: potato
     },
-    {
-      question: "Which vegetables do you have?",
-      item1:{
-        value: "tomatoes",
-        image:placeholder,},
-      item2: {
-        value:"paprika",
-        image:paprika},
-        item3: {
-          value:"peas",
-          image:peas},
-          item4: {
-            value:"pumpkin",
-            image:abobora}
-      
+    item2: {
+      value: "spaghetti",
+      image: pasta
     },
-    {
-      question: "Which dairy products do you have?",
-      item1:{
-        value: "swiss cheese",
-        image:cheese,},
-      item2: {
-        value:" low fat milk",
-        image:milk},
-        item3: {
-          value:"free range egg",
-          image:eggs},
-          item4: {
-            value:"light butter",
-            image:butter}
+    item3: {
+      value: "jasmine rice",
+      image: rice
     },
-    {
-      question: "Which dairy products do you have?",
-      item1:{
-        value: "red chili",
-        image:chili,},
-      item2: {
-        value:"garlic",
-        image:garlic},
-        item3: {
-          value:"ginger",
-          image:ginger},
-          item4: {
-            value:"red onion",
-            image:onions}
-    },
-    {
-      question: "Which dairy products do you have?",
-      item1:{
-        value: "whole chicken",
-        image:chicken,},
-      item2: {
-        value:"lean pork tenderloin",
-        image:pork},
-        item3: {
-          value:"beef",
-          image:beef},
-          item4: {
-            value:"salmon fillet",
-            image:salmon}
+    item4: {
+      value: "flour",
+      image: flour
     }
-    
-  ];
-  export default buzzQuestions
+  },
+  {
+    question: "Which of these vegetables do you have?",
+    item1: {
+      value: "tomatoes",
+      image: placeholder
+    },
+    item2: {
+      value: "paprika",
+      image: paprika
+    },
+    item3: {
+      value: "peas",
+      image: peas
+    },
+    item4: {
+      value: "pumpkin",
+      image: abobora
+    }
+  },
+  {
+    question: "Which of these dairy products do you have?",
+    item1: {
+      value: "swiss cheese",
+      image: cheese
+    },
+    item2: {
+      value: " low fat milk",
+      image: milk
+    },
+    item3: {
+      value: "free range egg",
+      image: eggs
+    },
+    item4: {
+      value: "light butter",
+      image: butter
+    }
+  },
+  {
+    question: "Which dairy products do you have?",
+    item1: {
+      value: "red chili",
+      image: chili
+    },
+    item2: {
+      value: "garlic",
+      image: garlic
+    },
+    item3: {
+      value: "ginger",
+      image: ginger
+    },
+    item4: {
+      value: "red onion",
+      image: onions
+    }
+  },
+  {
+    question: "Which dairy products do you have?",
+    item1: {
+      value: "whole chicken",
+      image: chicken
+    },
+    item2: {
+      value: "lean pork tenderloin",
+      image: pork
+    },
+    item3: {
+      value: "beef",
+      image: beef
+    },
+    item4: {
+      value: "salmon fillet",
+      image: salmon
+    }
+  }
+];
+export default buzzQuestions;
