308030da024be06a7d3780b088887c9c2ac6008c
anu radha <anu.sivasubramanian@gmail.com>
2020-03-11 13:53:47 +0100

    made recipe page responsive

--- src/component/Recipe.js
+++ src/component/Recipe.js
@@ -1,60 +1,67 @@
 import React from "react";
 import MissedIngredients from "./MissedIngredients";
 import "./css/recipe.css";
+import Basket from "./images/shopping-basket.svg";
 class Recipe extends React.Component {
   state = {
     missedIngredMount: false
   };
 
-<<<<<<< HEAD
-  handlremissedIngredients = () => {
-    this.setState({ missedIngredMount: true });
+  togglermissedIngredients = () => {
+    this.setState({ missedIngredMount: !this.state.missedIngredMount });
   };
-=======
-  togglermissedIngredients=()=>{
-    this.setState({missedIngredMount:!this.state.missedIngredMount})
-  }
->>>>>>> a8efbfde93704a6b952d270e3c60d3544ed885eb
 
   render() {
     return (
       <div className="recipe_body">
-<<<<<<< HEAD
+        {this.state.missedIngredMount ? (
+          <MissedIngredients
+            togglermissedIngredients={this.togglermissedIngredients}
+            missedIngredients={this.props.missedIngredients}
+          />
+        ) : null}
         <div className="wrap-recipe-content">
           <figure className="recipe-image">
             <img src={this.props.recipeImage} alt="recipe" />
           </figure>
-=======
-                 <button onClick={this.togglermissedIngredients}> open </button>
-       {this.state.missedIngredMount? <MissedIngredients togglermissedIngredients={this.togglermissedIngredients} missedIngredients={this.props.missedIngredients}/>: null}
-        
-      <div className="wrap-recipe-content">
-        
-        
-        <figure className="recipe-image">
-          <img src={this.props.recipeImage} alt="recipe"/>
-                  </figure>
->>>>>>> a8efbfde93704a6b952d270e3c60d3544ed885eb
 
           <div className="recipe-instructions">
             <h3>{this.props.recipeTitle}</h3>
-            {/*          <button onClick={this.handlremissedIngredients}> open </button>
-             */}{" "}
-            {this.state.missedIngredMount ? (
-              <MissedIngredients
-                missedIngredients={this.props.missedIngredients}
-              />
-            ) : null}
-            <a href={this.props.recipeLink}>{this.props.recipeLink}</a>
+            <a className="link_recipe" href={this.props.recipeLink}>
+              Click here for the recipe
+            </a>
             {this.props.recipeCreditText ? (
-              <p>This recipe is from - {this.props.recipeCreditText}</p>
+              <p>
+                This recipe is from -{" "}
+                <span className="credit_text">
+                  {this.props.recipeCreditText}
+                </span>
+              </p>
             ) : null}
+
+            <div className="icons_container">
+              {" "}
+              <img
+                className="basket_icon"
+                src={Basket}
+                onClick={this.togglermissedIngredients}
+                alt="shopping basket"
+              />
+              <img
+                className="home_icon"
+                src={Basket}
+                onClick={this.togglermissedIngredients}
+                alt="shopping basket"
+              />
+              <img
+                className="contact_icon"
+                src={Basket}
+                onClick={this.togglermissedIngredients}
+                alt="shopping basket"
+              />
+            </div>
           </div>
         </div>
-        {/* <MissedIngredients recipeData={this.props.data} /> */}
-        {/*  <button onclick={this.handlremissedIngredients}> open </button>
-       {this.state.missedIngredMount? <MissedIngredients recipeData={this.props.data}/>: null}
-        */}
       </div>
     );
   }
