5eb8006e9ec357aba837280c98539fd80512f8f8
anu radha <anu.sivasubramanian@gmail.com>
2020-03-11 14:47:31 +0100

    implementing env

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import Home from "./component/Home";
+console.log(process.env.REACT_APP_API_KEY);
 function App() {
   return (
     <div>
-      <Home/>
-    {/*     <footer>
+      <Home />
+      {/*     <footer>
         <a href="https://spoonacular.com/food-api">
           This project uses Spoonacular API
         </a>
