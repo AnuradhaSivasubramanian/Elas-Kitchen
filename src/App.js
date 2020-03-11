e40af5b09ef2b2377d30b1237908ff8d6510fb71
anu radha <anu.sivasubramanian@gmail.com>
2020-03-11 16:27:14 +0100

    added key into env file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import "./App.css";
 import Home from "./component/Home";
-console.log(process.env.REACT_APP_API_KEY);
+
 function App() {
   return (
     <div>
       <Home />
-      {/*     <footer>
-        <a href="https://spoonacular.com/food-api">
-          This project uses Spoonacular API
-        </a>
-      </footer> */}
     </div>
   );
 }
